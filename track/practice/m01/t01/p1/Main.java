bbb70641bbccc6b6801249fa864385c25f9d7e13
Bhanu509 <reddybhanuprakash77@gmail.com>
2026-08-03 22:57:37 +0530

    program1

---
+++ track/practice/m01/t01/p1/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        String learnerName = "Ananya";
+        int completedProblems = 25;
+        double practiceHours = 2.5;
+        boolean projectCompleted = true;
+        System.out.println("Learner: " + learnerName);
+        System.out.println("completed Problems: " + completedProblems);
+        System.out.println("practice Hours: " + practiceHours);
+        System.out.println("project Completed: " + projectCompleted);
+
+    }
+}
