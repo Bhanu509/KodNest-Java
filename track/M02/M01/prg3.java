d4a2caae674e1126c01f9f5a990fd7ce77b2e141
Bhanu509 <reddybhanuprakash77@gmail.com>
2026-08-04 12:34:49 +0530

    decision program

---
+++ track/M02/M01/prg3.java
@@ -0,0 +1,14 @@
+
+public class prg3 {
+
+    public static void main(String[] args) {
+        int num = -2;
+        if (num > 0) {
+            System.out.println("positive number");
+        } else if (num == 0) {
+            System.out.println("zero");
+        } else {
+            System.out.println("negative number");
+        }
+    }
+}
